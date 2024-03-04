6ae7b821aa15b672d389ac0c1ea1a3f2d2cbc6e3
Gowtham1802 <gowthama7575@gmail.com>
2024-03-04 13:48:57 +0530

    add

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,26 +1,64 @@
 import React from "react";
+import Slider from "react-slick";
 import brand1 from "../../../src/assets/images/brand/br-1.png";
 import brand2 from "../../../src/assets/images/brand/br-2.png";
 import brand3 from "../../../src/assets/images/brand/br-3.png";
 import brand4 from "../../../src/assets/images/brand/br-4.png";
 import brand5 from "../../../src/assets/images/brand/br-5.png";
+import Heading from "../Shared/Heading";
 
 const Partners = () => {
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "ease-in",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+  };
+
   return (
-    <div
-      data-aos="zoom-out"
-      className="py-8 mt-24 hidden md:block bg-gray-200 dark:bg-white/10"
-    >
-      <div className="container">
-        <div className="grid grid-cols-5 gap-3 place-items-center opacity-50">
-          <img src={brand1} alt="brand" className="w-[80px] dark:invert" />
-          <img src={brand2} alt="brand" className="w-[80px] dark:invert" />
-          <img src={brand3} alt="brand" className="w-[80px] dark:invert" />
-          <img src={brand4} alt="brand" className="w-[80px] dark:invert" />
-          <img src={brand5} alt="brand" className="w-[80px] dark:invert" />
+    <>
+      <Heading title="Our Partners" />
+      <div
+        data-aos="zoom-out"
+        className="py-8 mt-4  md:block bg-gray-200 dark:bg-white/10"
+      >
+        <div className="container">
+          <Slider {...settings}>
+            <img
+              src={brand1}
+              alt="brand"
+              className="w-[80px] h-[80px] object-contain dark:invert"
+            />
+            <img
+              src={brand2}
+              alt="brand"
+              className="w-[80px] h-[80px] object-contain dark:invert"
+            />
+            <img
+              src={brand3}
+              alt="brand"
+              className="w-[80px] h-[80px] object-contain dark:invert"
+            />
+            <img
+              src={brand4}
+              alt="brand"
+              className="w-[80px] h-[80px] object-contain dark:invert"
+            />
+            <img
+              src={brand5}
+              alt="brand"
+              className="w-[80px] h-[80px] object-contain dark:invert"
+            />
+          </Slider>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
